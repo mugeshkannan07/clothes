acf7735b89ee03eb109fa4075d13e11246ef81a5
Mugesh Kannan <mugesh71536@gmail.com>
2025-04-24 11:01:18 +0530

    image

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,14 @@ app.use("/api/clothes", allClothesRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 
+// Frontend Routes
+const frontendPath = path.join(__dirname, "client", "dist"); 
+app.use(express.static(frontendPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
